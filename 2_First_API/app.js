93ffeaceb6dcab959e671dda7d43d96c4e04d81c
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-02-06 14:02:21 +0100

    created the very first api

---
+++ 2_First_API/app.js
@@ -0,0 +1,8 @@
+var express = require("express"); //import library. in js, import is called require. node modules know where to look at it
+var app = express(); //instantiate express
+
+
+app.listen(3000); //create a server with port number 3000. one of the most used port for production. 
+
+
+//=> created a valid API

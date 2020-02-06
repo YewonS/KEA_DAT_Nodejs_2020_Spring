2eb4abed5a920760a93dbe2a7c9204e361714537
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-02-06 10:34:17 +0100

    Added exercises 3-5

---
+++ 1_intro/Intro_Exercises/2.Strings.js
@@ -0,0 +1,35 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+// add those two numbers and show the result
+var result3 = +(numberOne) + +(numberTwo);
+console.log(result3);
+// you cannot touch line 1 neither line 2
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+var numberOne = "1.10";
+var numberTwo = "2.30";
+
+var result4 = (+(numberOne) + +(numberTwo)).toFixed(2);
+console.log(result4);
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+var one = 10;
+var two = 45;
+var three = 98;
+
+// Show in the console the avg. with 5 decimals
+var result5 = ((one + two + three) / 3).toFixed(5);
+console.log(result5);
+// --------------------------------------
+

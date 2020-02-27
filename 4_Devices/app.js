dc67925ae0a4bac81061cf3670933ca7cf085942
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-02-27 13:51:54 +0100

    devices initiated

--- 4_Devices/app.js
+++ 4_Devices/app.js
@@ -1,5 +1,11 @@
 const app = require("express")()
 
+let devices = [
+    {id: 1, type: "macbook"},
+    {id: 2, type: "iphone"},
+    {id: 3, type: "applewatch"},
+    {id: 4, type: "airpods"},
+    {id: 5, type: "ipad"}]
 
 app.get("/", (req, res) => {
 
@@ -8,16 +14,41 @@ app.get("/", (req, res) => {
         description: "This is a new API for devices"
     }
 
-    res.send(thisAPI);
-});
+    return res.send(thisAPI)
+})
+
+app.get("/devices", (req, res) => {
+
+    return res.send({
+        devices: devices
+    })
+})
+
+app.get("/devices/:id", (req, res) => {
+    
+    const device = devices.find(device => device.id === Number(req.params.id)) // comparing the id of the device with the number I sent through that are parsed into number
+
+    return res.send({
+        devices: devices
+    })
+
+})
+
+app.post("/test", (req, res) => {
+    console.log("test")
+    res.send({ })
+})
 
 
-app.listen(3000, error => { 
+
+
+
+const server = app.listen(3000, error => { 
     
     if(error) { 
         console.log(error);
     }
 
-    console.log("Server is running on port", 3000);
+    console.log("Server is running on port", server.address().port);
 
 })

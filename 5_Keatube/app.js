e8a817540edbda36f1509dfea87ea5906bd36bcc
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-03-05 13:52:21 +0100

    started video

---
+++ 5_Keatube/app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json()) // to get responses from post
+app.use(express.urlencoded({ extended: false })) // if you submit a form
+const port = process.env.PORT ? process.env.PORT : 3000 // if it's undefined, run on 3000
+app.use(express.static('public'))
+app.use(express.static('videos'))
+
+
+app.get("/video/:videoid", (req, res) => {
+
+    return res.sendFile(__dirname + "/public/video.html")
+})
+
+
+const server = app.listen(port, error => { 
+    
+    if(error) { 
+        console.log(error);
+    }
+
+    console.log("Server is running on port", server.address().port);
+
+})

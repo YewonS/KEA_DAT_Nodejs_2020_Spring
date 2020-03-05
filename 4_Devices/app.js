a64a8e1d3d7b8146ed68e92fdaf58bba86c7e150
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-03-05 11:41:43 +0100

    http request completed

--- 4_Devices/app.js
+++ 4_Devices/app.js
@@ -1,4 +1,8 @@
-const app = require("express")()
+const express = require("express");
+const app = express();
+
+app.use(express.json()) // to get responses from post
+app.use(express.urlencoded({ extended: false })) // if you submit a form
 
 let devices = [
     {id: 1, type: "macbook"},
@@ -6,6 +10,8 @@ let devices = [
     {id: 3, type: "applewatch"},
     {id: 4, type: "airpods"},
     {id: 5, type: "ipad"}]
+
+let nextDeviceID = 6
 
 app.get("/", (req, res) => {
 
@@ -20,7 +26,7 @@ app.get("/", (req, res) => {
 app.get("/devices", (req, res) => {
 
     return res.send({
-        devices: devices
+        response: devices
     })
 })
 
@@ -29,18 +35,59 @@ app.get("/devices/:id", (req, res) => {
     const device = devices.find(device => device.id === Number(req.params.id)) // comparing the id of the device with the number I sent through that are parsed into number
 
     return res.send({
-        devices: devices
+        response: devices
     })
 
 })
 
-app.post("/test", (req, res) => {
-    console.log("test")
-    res.send({ })
+// app.post("/test", (req, res) => {
+//     console.log(req.body)
+//     return res.send({ })
+// })
+
+app.post("/testtwo", (req, res) => {
+    //console.log({ "body": req.body})
+    return res.send({ response: req.body })
 })
 
+app.post("/devices", (req, res) => {
 
+    let newDevice = req.body
+    if (!newDevice.type) {
+        return res.status(400).send({ message: "Missing the device type" }) // send the status
+    }
+    newDevice.id = nextDeviceID
+    devices.push(newDevice)
+    nextDeviceID ++
+    return res.send({ response: newDevice })
 
+})
+
+app.put("/devices/:id", (req, res) => {
+
+    const foundIndex = devices.findIndex(device => device.id === Number(req.params.id)) // find index that fulfills the condition. it's like filter
+    delete req.body.id // just in case client sends the id. we don't wanna mess around with the id
+    
+    const changeDevice = { ...devices[foundIndex], ...req.body }
+    devices[foundIndex] = changeDevice
+
+    // changing types only
+    // let changeDevice = req.body
+    // if (!changeDevice.type) {
+    //     return res.status(400).send({ message: "Missing the device type "})
+    // }
+    // devices[device.id - 1].type = changeDevice.type
+
+    return res.send({ response: changeDevice })
+
+})
+
+app.delete("/devices/:id", (req, res) => {
+
+    devices = devices.filter(device => device.id !== Number(req.params.id))
+    return res.send({ response: devices })
+
+})
 
 
 const server = app.listen(3000, error => { 

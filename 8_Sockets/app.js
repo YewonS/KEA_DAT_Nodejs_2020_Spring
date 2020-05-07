37b2685a0130c2f82867c28eb9f341057191d34c
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-05-07 13:30:35 +0200

    last class for node

--- 8_Sockets/app.js
+++ 8_Sockets/app.js
@@ -3,21 +3,40 @@ const server = require('http').createServer(app); // http is a module that exist
 
 const io = require('socket.io')(server);
 
+const escape = require('escape-html');
+const helmet = require('helmet');
+app.use(helmet());
+
 io.on('connection', socket => {
     // console.log("socket joined", socket.id);
     // socket.on('disconnect', () => {
     //     console.log("socket left", socket.id);
     // });
 
-    socket.on("I'm thinking about this", data => {
-        console.log(data.thoughts);
+    socket.on("I'm thinking about this", ({ thoughts }) => {
+        // sends out to all clients
+        io.emit("Someone said,", { thoughts: escape(thoughts) });
+
+        // sends back to the client 
+        //socket.emit("Someone said," , { thoughts })
+
+        // sends out to all clients except the client itself that sends it
+        //socket.broadcast.emit("Someone said,", { thoughts })
     });
 
+    socket.on('color', ({ color }) => {
+        io.emit('color-change', { color: escape(color) })
+    });
+    
 });
 
 
 app.get("/", (req, res) => {
     return res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get("/colorpicker", (req, res) => {
+    return res.sendFile(`${__dirname}/colorPicker.html`)
 });
 
 server.listen(3000);
